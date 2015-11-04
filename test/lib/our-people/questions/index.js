348b36cf76f258218e9142a81f7743e77f63eb77
Jesse Earle <earle.jesse@gmail.com>
2015-11-04 11:28:56 -0500

    rebased jimits url validator into branch

--- test/lib/our-people/questions/index.js
+++ test/lib/our-people/questions/index.js
@@ -9,7 +9,7 @@ import questions from 'our-people/questions';
 describe('questions', () => {
 
 
-  it('questions should eventually be an array', () =>
+  it('questions should be an array', () =>
 
     expect(questions).to.be.an('array'));
 
