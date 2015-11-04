be0454d4adfce3edd0e3f8ffd3db5eb3e8650f27
Jimit Patel <jdp59@njit.edu>
2015-11-04 13:06:22 -0500

    twiiter question separated and tested

---
+++ test/helpers/twitter-handle.js
@@ -0,0 +1,2 @@
+
+export const validTwitterHandle = 'jimitpatel9';
---
+++ test/lib/our-people/questions/twitter.js
@@ -0,0 +1,31 @@
+
+
+import { expect } from 'chai';
+
+
+import { filter } from 'our-people/questions/twitter';
+import validTwitterHandle from '../../../helpers/twitter-handle.js';
+
+describe('twitter question', () => {
+
+
+
+    describe('filter', () => {
+
+
+        function filterOutput () {
+
+            return filter(validTwitterHandle);
+
+        }
+
+
+        it('should create create a valid link with a valid twitter handle', () =>
+
+            expect(filterOutput()).to.be.a('string'));
+
+
+    });
+
+
+});
