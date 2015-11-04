3691ddb43510bb3e8d8ba626af090b67a57b32fa
Jimit Patel <jdp59@njit.edu>
2015-11-04 12:27:04 -0500

    linkedin question tested and separated

---
+++ test/lib/our-people/questions/linkedin.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+
+
+import { validUrl , invalidUrl } from '../../../helpers/url';
+import { validate , invalidatedMessage } from 'our-people/questions/linkedin';
+
+
+describe('linkedin question', () => {
+
+    describe('validate', () => {
+
+        function validated(){
+
+            return validate(validUrl);
+
+        }
+        function invalidated(){
+
+            return validate(invalidUrl);
+
+        }
+
+        it('should validate with a valid linkedin url by returning true', () => {
+
+            expect(validated()).to.be.true;
+
+        });
+
+        it(`should invalidate with a invalid linkedin url by returning string "${invalidatedMessage}"`, () => {
+
+            expect(invalidated()).to.be.equal(invalidatedMessage);
+
+        });
+
+    })
+
+
+});
