c1aa7015bfaf1a510cddb3352be98c01e9ff07b1
Jimit Patel <jdp59@njit.edu>
2015-11-04 16:31:50 -0500

    updated the phonenumber question testing and separation

---
+++ test/lib/our-people/questions/phone-number.js
@@ -0,0 +1,42 @@
+
+
+import { expect } from 'chai';
+
+
+import { validate, invalidatedMessage } from 'our-people/questions/phone-number';
+import { validPhoneNumber, invalidPhoneNumber } from '../../../helpers/phone-number';
+
+describe('phone number question', () => {
+
+
+    describe('validate', () => {
+
+        function validated () {
+
+            return validate(validPhoneNumber);
+
+        };
+
+
+        function invalidated () {
+
+            return validate(invalidPhoneNumber);
+
+        };
+
+        it('should validate with a valid phone number by returning true', () => {
+
+            expect(validated()).to.be.true;
+
+        });
+
+
+        it(`should invalidate with a invalid phone number by returning string ${invalidatedMessage}`, () => {
+
+            expect(invalidated()).to.be.equal(invalidatedMessage);
+
+        });
+    });
+
+
+});
