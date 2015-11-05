f429c4647350ab9ac8440404889ea9710bf785ca
Jesse Earle <earle.jesse@gmail.com>
2015-11-05 14:22:29 -0500

    added helper functions to questions, added untested web api

--- test/lib/our-people/questions/index.js
+++ test/lib/our-people/questions/index.js
@@ -3,15 +3,37 @@
 import { expect } from 'chai';
 
 
-import questions from 'our-people/questions';
+import questions, { questionByName, questionValidatorByName } from 'our-people/questions';
 
 
-describe('questions', () => {
+describe.only('questions', () => {
 
 
   it('questions should be an array', () =>
 
     expect(questions).to.be.an('array'));
+
+
+  describe('questionByName', () => {
+
+
+    it('should return a question object', () =>
+
+      expect(questionByName('name')).to.be.a('object'));
+
+
+  });
+
+
+  describe('questionValidatorByName', () => {
+  
+  
+    it('should return a validator function from the question', () =>
+
+      expect(questionValidatorByName('name')).to.be.a('function'));
+  
+
+  });
 
 
 });
