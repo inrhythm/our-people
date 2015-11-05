34cadc06502e87a526c31ca68fe8adadf2a27293
Jesse Earle <earle.jesse@gmail.com>
2015-11-05 10:18:55 -0500

    updated spacing

--- test/lib/our-people/questions/phone-number.js
+++ test/lib/our-people/questions/phone-number.js
@@ -6,37 +6,38 @@ import { expect } from 'chai';
 import { validate, invalidatedMessage } from 'our-people/questions/phone-number';
 import { validPhoneNumber, invalidPhoneNumber } from '../../../helpers/phone-number';
 
+
 describe('phone number question', () => {
 
 
-    describe('validate', () => {
-
-        function validated () {
-
-            return validate(validPhoneNumber);
-
-        };
+  describe('validate', () => {
 
 
-        function invalidated () {
+    function validated () {
 
-            return validate(invalidPhoneNumber);
+      return validate(validPhoneNumber);
 
-        };
-
-        it('should validate with a valid phone number by returning true', () => {
-
-            expect(validated()).to.be.true;
-
-        });
+    }
 
 
-        it(`should invalidate with a invalid phone number by returning string ${invalidatedMessage}`, () => {
+    function invalidated () {
 
-            expect(invalidated()).to.be.equal(invalidatedMessage);
+      return validate(invalidPhoneNumber);
 
-        });
-    });
+    }
+
+
+    it('should validate with a valid phone number by returning true', () =>
+
+      expect(validated()).to.be.true)
+
+
+    it(`should invalidate with a invalid phone number by returning string ${invalidatedMessage}`, () =>
+
+      expect(invalidated()).to.be.equal(invalidatedMessage));
+
+
+  });
 
 
 });
